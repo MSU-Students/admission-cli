c90b96200a7ba5e29c1afef795064469ac66c0ea
Lucman Abdulrachman <fencim@gmail.com>
2021-02-26 15:03:48 +0800

    list all students

--- admission.js
+++ admission.js
@@ -2,11 +2,7 @@ const level = require('level');
 
 (function main() {
     const db = connectToDatabase('admssion-db');
-    const studentId = "1234";
-    const studentName = "Luffy";
-    const studentAge = 20;
-    const studentAddress = "wano";
-    acceptStudent(db, studentId, studentName, studentAge, studentAddress);
+    listAllStudents(db);
 }());
 
 function connectToDatabase(dbName) {
@@ -25,3 +21,31 @@ async function acceptStudent(db, studentId, studentName, studentAge, studentAddr
     };
     return  db.put(studentId,  studentInfo);
 }
+
+async function getNext(iterator) {
+    return new Promise((resolve, reject) => {
+        iterator.next((err, key, value) => {
+            if (err) {
+                reject(err);
+            } else if (key && value) {
+                resolve({key: key, value: value});
+            } else {
+                iterator.end(() => {
+                    resolve(undefined);
+                })
+            }
+        });
+    })
+}
+async function listAllStudents(db) {
+    let iterator = db.iterator();
+    let nextKeyValue;
+    do {
+        nextKeyValue = await getNext(iterator);
+        if (nextKeyValue) {
+            const student = nextKeyValue.value;
+            console.log(student.id, student.name, student.address);
+        }
+    } while (nextKeyValue);
+}
+
