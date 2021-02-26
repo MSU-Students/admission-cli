f47aa94b07fc071c062be6565d993012376eaa31
Lucman Abdulrachman <fencim@gmail.com>
2021-02-26 14:53:22 +0800

    accept student

--- admission.js
+++ admission.js
@@ -1,7 +1,12 @@
 const level = require('level');
 
 (function main() {
-    connectToDatabase('admssion-db');
+    const db = connectToDatabase('admssion-db');
+    const studentId = "1234";
+    const studentName = "Luffy";
+    const studentAge = 20;
+    const studentAddress = "wano";
+    acceptStudent(db, studentId, studentName, studentAge, studentAddress);
 }());
 
 function connectToDatabase(dbName) {
@@ -9,4 +14,14 @@ function connectToDatabase(dbName) {
         valueEncoding: 'json'
     };
     return level(dbName, options);
+}
+
+async function acceptStudent(db, studentId, studentName, studentAge, studentAddress) {
+    const studentInfo = {
+        id : studentId,
+        name: studentName,
+        age: studentAge,
+        address: studentAddress
+    };
+    return  db.put(studentId,  studentInfo);
 }
