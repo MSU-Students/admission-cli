b2f6437faa8f8a0a8d0c3068213f2a9b73b0f9cc
Lucman Abdulrachman <fencim@gmail.com>
2021-02-26 15:28:30 +0800

    schedule interview

--- admission.js
+++ admission.js
@@ -3,19 +3,25 @@ const level = require('level');
 (function main() {
     const db = connectToDatabase('admssion-db');
     const args = process.argv.splice(2);
+    let studentId;
     switch (args[0]) {
         case 'accept':
-            const studentId = args[1];
+            studentId = args[1];
             const studentName = args[2];
             const studentAge = args[3];
             const studentAddress = args[4];
             acceptStudent(db, studentId, studentName, studentAge, studentAddress);
             break;
+        case 'sched-interview':
+            studentId = args[1];
+            const interviewDate = args[2];            
+            scheduleInterview(db, studentId, interviewDate);
+            break;
         case 'list':
             listAllStudents(db);
             break;
         default:
-            console.log('accept', 'list');
+            console.log('Commands:', 'accept', 'list', 'sched-interview');
     }
 }());
 
@@ -58,8 +64,17 @@ async function listAllStudents(db) {
         nextKeyValue = await getNext(iterator);
         if (nextKeyValue) {
             const student = nextKeyValue.value;
-            console.log(student.id, student.name, student.age, student.address);
+            console.log(student.id, student.name, student.age, student.address, student.interviewSched);
         }
     } while (nextKeyValue);
 }
 
+async function scheduleInterview(db, studentId, interviewDate) {
+    try {
+        const student =  await db.get(studentId);
+        student.interviewSched = interviewDate;
+        db.put(studentId, student);
+    } catch (error) {
+        console.log('invalid id', studentId);
+    }
+}
